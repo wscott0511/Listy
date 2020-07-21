442974aa79d7bd332c1f8759b793330b62c599c2
Ryan Scott <williamscott0511@gmail.com>
2020-07-21 10:15:19 -0700

    Xcode project initial files

---
+++ Listy/Listy/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Listy
+//
+//  Created by William Scott on 7/21/20.
+//  Copyright © 2020 William Scott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
